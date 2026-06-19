fc09b5427a29e44e26868e7aac6929e7fecfcb67
farhanezi <esafitrioktorini@gmail.com>
2026-06-19 10:32:59 +0700

    hi

--- web-sekolah/routes/web.php
+++ web-sekolah/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('home');
-=======
-    return view('general/landing_page');
->>>>>>> 77979ba2d8885462d84c597e4b86b7e1491b771b
 });
